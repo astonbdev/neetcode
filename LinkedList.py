038afd5a8301723afd39e9d497b6b70f8d084638
Brian Aston <astonb.dev@gmail.com>
2025-01-06 15:07:50 -0800

    Pythonifying

---
+++ LinkedList.py
@@ -0,0 +1,19 @@
+class LinkedList:
+
+    def __init__(self):
+
+
+    def get(self, index: int) -> int:
+
+
+    def insert_head(self, val: int) -> None:
+
+
+    def insert_tail(self, val: int) -> None:
+
+
+    def remove(self, index: int) -> bool:
+
+
+    def get_values(self) -> List[int]:
+
