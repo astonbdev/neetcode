c3340e575e1f9909c13fc8b824a7330380f58837
Brian Aston <astonb.dev@gmail.com>
2025-01-06 15:33:57 -0800

    Adding remove

--- LinkedList.py
+++ LinkedList.py
@@ -1,7 +1,7 @@
 class LinkedList:
 
-    def __init__(self, node):
-        self.head = node
+    def __init__(self):
+        return None
 
     def get(self, index: int) -> int:
         count = 0
@@ -15,13 +15,22 @@ class LinkedList:
         return curr_node.val
 
     def insert_head(self, val: int) -> None:
+        if self.head is None:
+            self.head = Node(val)
+            return
+
         new_node = Node(val)
         new_node.next = self.head
         self.head = new_node
 
         return None
 
+    # [1,2,3,4] -> 5
     def insert_tail(self, val: int) -> None:
+        if self.head is None:
+            self.head = Node(val)
+            return
+
         new_node = Node(val)
         curr_node = self.head
 
@@ -32,10 +41,42 @@ class LinkedList:
         return None
 
     def remove(self, index: int) -> bool:
-        return None
+        # case of None list
+        if (self.head is None):
+            return False
+
+        # case of list with exactly one element
+        if (index == 0):
+            if (self.head.next is None):
+                self.head = None
+                return True
+        else:
+            self.head = self.head.next
+            return True
+
+        count = 0
+        curr_node = self.head
+
+        while (count < index - 1 and curr_node.next is not None):
+            curr_node = curr_node.next
+            count += 1
+
+        if (curr_node.next is None):
+            return False
+        curr_node.next = curr_node.next.next
+
+        return True
 
     def get_values(self) -> List[int]:
-        return None
+        vals = []
+        curr_node = self.head
+        vals.append(self.head.val)
+
+        while (curr_node is not None):
+            curr_node = curr_node.next
+            vals.append(curr_node.val)
+
+        return vals
 
 
 class Node:
