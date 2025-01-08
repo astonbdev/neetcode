0fbf835f9733dedd5da1c6bb3e173bea40615605
Brian Aston <astonb.dev@gmail.com>
2025-01-07 19:39:00 -0800

    initial pass

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,7 +1,35 @@
 # Definition for a pair.
-# class Pair:
-#     def __init__(self, key: int, value: str):
-#         self.key = key
-#         self.value = value
+class Pair:
+    def __init__(self, key: int, value: str):
+        self.key = key
+        self.value = value
+
+
 class Solution:
-    def insertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+    def insertionSort(self, pairs: list[Pair]) -> list[list[Pair]]:
+
+        length = len(pairs)
+        out = []
+        out.append(pairs.copy())
+
+        # [2,9,5]
+        for i in range(1, length + 1):
+            curr_pair = pairs[i]
+            count = i
+
+            while (count > 0):
+                prev_pair = pairs[i-1]
+                curr_key = curr_pair[0]
+                prev_key = prev_pair[0]
+
+                if (curr_key < prev_key):
+                    pairs[i] = prev_pair
+                    pairs[i-1] = curr_pair
+                else:
+                    break
+
+                count -= 1
+
+            out.append(pairs.copy())
+
+        return out
