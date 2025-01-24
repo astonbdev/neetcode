5d8f83c1f5f6789d1364e910de13d4a7d1963747
Brian Aston <astonb.dev@gmail.com>
2025-01-24 14:19:10 -0800

    Worked on an iteration solution, but ultimately looked at answer, a lot simpler than I had though. I had all the right ideas, just got trapped in my own headspace

--- addBinary.js
+++ addBinary.js
@@ -4,6 +4,8 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
+  if(a === "0" && b === "0") return "0";
+  
   const intA = binaryToInt(a);
   const intB = binaryToInt(b);
 
@@ -11,6 +13,40 @@ var addBinary = function(a, b) {
 
   return intToBinary(sum);
 };
+
+function addBinaryDumb (a,b){
+  const long = a.length >= b.length ? a : b;
+  const short = a.length < b.length ? a : b;
+
+  let out = long;
+
+  //start by iterating over out shorter string from the end
+  for(let i = short.length - 1; i >= 0; i--){
+    //using the value of the current iteration character, iterate over the longer string
+    //we will flip 1 to 0 or 0 to 1 based on the results of what is happening
+    let carry = short[i] === "1" ? true : false;
+
+    for(let j = ((long.length - 1) - i); j >= 0; j--){
+      if(carry){
+        if(long[j] === "1"){
+          out[j] = "0";
+        }
+        else{
+          carry = false
+          out[j] = "1"
+          break;
+        }
+      }
+    }
+
+    out = long;
+    if(carry) out = "1" + out;
+  }
+
+  return out;
+}
+
+addBinaryDumb("1", "10");
 
 function binaryToInt(binaryStr) {
   //iterate over string starting from the end
@@ -21,10 +57,10 @@ function binaryToInt(binaryStr) {
   //if the value is 0, skip
 
   let exponent = 0;
-  let sum = 0
+  let sum = BigInt(0);
 
   for(let i = binaryStr.length - 1; i >= 0; i-- ){
-      if(binaryStr[i] === "1") sum += 2**exponent;
+      if(binaryStr[i] === "1") sum += BigInt(2**exponent);
       exponent += 1;
   }
 
@@ -42,14 +78,14 @@ function intToBinary(value) {
 
   //this gets us the maximum exponent we would need to achieve the input value
   //we can then derive the value of our binary string starting with this exponent
-  let exponent = Math.floor(Math.log2(value));
+  let exponent = Math.floor(Math.log2(value.toString()));
   let binaryStr = "";
   let workingValue = value;
 
   for(let i = exponent; i >= 0; i--){
     if(2**i <= workingValue){
       binaryStr += "1";
-      workingValue -= 2**i;
+      workingValue -= BigInt(2**i);
     } 
     else{
       binaryStr += "0";
@@ -58,3 +94,25 @@ function intToBinary(value) {
 
   return binaryStr;
 }
+
+
+var addBinary = function(a, b) {
+  let carry = 0;
+  let res = '';
+  let i = a.length - 1;
+  let j = b.length - 1;
+
+  while (i >= 0 || j >= 0 || carry) {
+      let sum = carry;
+      if (i >= 0) sum += parseInt(a[i]);
+      if (j >= 0) sum += parseInt(b[j]);
+
+      res = (sum % 2) + res;
+      carry = Math.floor(sum / 2);
+
+      i--;
+      j--;
+  }
+
+  return res;   
+};
