daa5e6cc95900369f3c95f2d7b663c2b09e1ef49
Brian Aston <astonb.dev@gmail.com>
2025-01-24 13:03:21 -0800

    working binary to int

---
+++ addBinary.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+  //start by converting the binary string to int
+  //add
+  //convert int to binary number
+};
+
+function binaryToInt(binaryStr) {
+  //iterate over string starting from the end
+  //keep a count of the current exponent.
+  //keep a count of the current value
+  //as we iterate, add to our tracked value, the value of the
+  //current exponent count by 2 if the value is 1
+  //if the value is 0, skip
+
+  let exponent = 0;
+  let sum = 0
+
+  for(let i = binaryStr.length - 1; i >= 0; i-- ){
+      if(binaryStr[i] === 1) sum += 2**exponent;
+  }
+
+  return sum;
+}
+
+function intToBinary() {}
