e4c283948051e5726571198430e2cefe69854e03
Brian Aston <astonb.dev@gmail.com>
2025-01-23 19:52:20 -0800

    Submitted plusOne.js

--- plusOne.js
+++ plusOne.js
@@ -15,6 +15,26 @@ var plusOne = function(digits) {
   //again for carry over
   //iterate until we have done this for all existing digits.
   //if we still have carry over, we then add one to the start of the array
-  
-    
+  //[1,2,3]
+  //[1,4,9]
+  //[9,9]
+
+  let currIdx = digits.length - 1;
+  let extra = 0;
+
+  while(currIdx >= 0){
+    extra = (1 + digits[currIdx]) % 10;
+    if(extra !== 0){
+      digits[currIdx] = extra;
+      break;
+    }
+    digits[currIdx] = 0;
+    //I've determined how to break the loop
+    //need to determine how to adjust the number
+    currIdx--;
+  }
+
+  if(extra === 0) digits.unshift(1);
+
+  return digits;
 };
