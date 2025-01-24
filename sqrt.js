060923f91acfc03dfdcdcf01303546202bc1bd6e
Brian Aston <astonb.dev@gmail.com>
2025-01-24 14:33:58 -0800

    Solution works, but there is a binarySearch version that is better

--- sqrt.js
+++ sqrt.js
@@ -1,3 +1,14 @@
 function sqrt(val){
-  
+  if(val === 0) return 0;
+  //start by finding the closest integer that when multiplied by itself is 
+  //LESS than the value, but not over it
+
+  let squareNum = 0;
+
+  for(let i = 1; i*i <= val;  i++){
+    squareNum = i;
+  }
+
+  return squareNum;
+
 }
